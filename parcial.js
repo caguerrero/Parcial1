130948e0d68747b9c51de536721f36501e044488
Carlos Guerrero <ca.guerrero@uniandes.edu.co>
2021-03-12 14:21:56 -0500

    Functionalidad 2 completa

--- parcial.js
+++ parcial.js
@@ -5,6 +5,7 @@ const url =
 
 fetch(url).then(res => res.json().then(res => {
     const space = document.getElementById("space");
+    let items = 0;
     Object.keys(res).forEach((key) => {
         const enlace = document.getElementById(res[key].name);
         enlace.onclick = () => {
@@ -28,13 +29,12 @@ fetch(url).then(res => res.json().then(res => {
             space.innerHTML = tableSelected;
             const btn = document.getElementsByClassName("add");
             const carrito = document.getElementById("carrito");
-            let items = 0;
             console.log(btn);
             for (let j of btn) {
                 console.log(j);
                 j.onclick = () => {
                     items++;
-                    carrito.innerHTML = items;
+                    carrito.innerHTML = items + " items";
                     console.log("Added to car");
                 };
             }
